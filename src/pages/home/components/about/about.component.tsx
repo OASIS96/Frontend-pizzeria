283f95718e0db0b464a4cdeae4c28f8616bba680
code9614 <code9614@gmail.com>
2021-01-29 23:59:17 -0500

    About addedm and stylized

---
+++ src/pages/home/components/about/about.component.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Icon1 from '../../../../assets/Icon-clock.svg';
+import Icon2 from '../../../../assets/Icon-nature.svg';
+import Icon3 from '../../../../assets/Icon-pizza.svg';
+import AboutBg from '../../../../assets/Image-about.jpg';
+import './about.component.scss';
+
+export const AboutComponent = () => {
+    return (
+      <div className="about_c_c" style={{backgroundImage: `url(${AboutBg})`}}>
+        <h1 className="title_about">Nuestras pizzas hablan por nosotros</h1>
+        <p className="descrip_about">
+          Praesent ipsum mauris, rutrum et dapibus ac, pulvinar sagittis lacus.
+          Etiam laoreet, tellus at commodo porttitor, arcu mauris tincidunt
+          massa, sed tempor massa metus quis sapien.{" "}
+        </p>
+        <div className="cards_container">
+          <div className="card">
+            <Icon1 />
+            <h2 className="title_card">Rapida Entrega</h2>
+            <p className="description_card">
+              Praesent ipsum mauris, rutrum et dapibus ac, pulvinar sagittis
+              lacus. Etiam laoreet, tellus at commodo porttitor, arcu mauris
+              tincidunt massa, sed tempor massa metus quis sapien.{" "}
+            </p>
+          </div>
+
+          <div className="card">
+            <Icon2 />
+            <h2 className="title_card">Rapida Entrega</h2>
+            <p className="description_card">
+              Praesent ipsum mauris, rutrum et dapibus ac, pulvinar sagittis
+              lacus. Etiam laoreet, tellus at commodo porttitor, arcu mauris
+              tincidunt massa, sed tempor massa metus quis sapien.{" "}
+            </p>
+          </div>
+
+          <div className="card">
+            <Icon3 />
+            <h2 className="title_card">Rapida Entrega</h2>
+            <p className="description_card">
+              Praesent ipsum mauris, rutrum et dapibus ac, pulvinar sagittis
+              lacus. Etiam laoreet, tellus at commodo porttitor, arcu mauris
+              tincidunt massa, sed tempor massa metus quis sapien.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+}
--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AboutComponent } from './components/about/about.component';
 import { BannerComponent } from './components/banner/banner.component';
 import './home.styles.scss';
 
@@ -6,6 +7,7 @@ export const HomePage = () => {
     return (
         <div className="home_p_c">
             <BannerComponent/>
+            <AboutComponent />
         </div>
     )
 }
