b00044b9611ccb506ebed2b330c96aeee10d1781
code9614 <code9614@gmail.com>
2021-01-31 00:35:02 -0500

    Slider eddited for flex animation

--- src/pages/home/components/slider/slider.component.tsx
+++ src/pages/home/components/slider/slider.component.tsx
@@ -1,60 +1,80 @@
-import React, { useEffect, useRef, useState } from 'react';
-import PizzaImg from '../.,/../../../../assets/Preview-slider.png';
-import BGImage from '../.,/../../../../assets/Background-slider.jpg';
-import ArrowSvg from '../.,/../../../../assets/arrow.svg';
-import './slider.component.scss';
+import React, { useEffect, useRef, useState } from "react";
+import PizzaImg from "../.,/../../../../assets/Preview-slider.png";
+import BGImage from "../.,/../../../../assets/Background-slider.jpg";
+import ArrowSvg from "../.,/../../../../assets/arrow.svg";
+import "./slider.component.scss";
 
 export const SliderComponent = () => {
-
   const sliderRef = useRef<HTMLDivElement>(null);
-  let indexSLide = 0;
-  const [slides_number, setslides_number] = useState(0)
+  const slider = sliderRef.current as HTMLDivElement;
+  let index = 1;
+  const [slides_number, setSlides_number] = useState(0);
 
-  const changeSlideLeft = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    if(indexSLide < 1) {
-      indexSLide = sliderRef.current?.children.length as number - 1;
-      for(let i=0;i< slides_number ;i++) {
-        sliderRef.current?.children[i].classList.remove('active');
-      }
-      sliderRef.current?.children[indexSLide].classList.add('active');
-    }else {
-      for(let i=0;i< slides_number ;i++) {
-        sliderRef.current?.children[i].classList.remove('active');
-      }
-      indexSLide--;
-      sliderRef.current?.children[indexSLide].classList.add('active');
+  const moveAllSliderLeft = (ev:globalThis.TransitionEvent) => {
+    if (index === 0) {
+      slider.style.transition = "";
+      index = slides_number - 2;
+      slider.style.transform = `translateX(-${index * 20}%)`;
     }
-    
   }
+
+  const moveAllSliderRigth = (ev:globalThis.TransitionEvent) => {
+    if (index === slides_number - 1) {
+      slider.style.transition = "";
+      index = 1;
+      slider.style.transform = `translateX(-${index * 20}%)`;
+    }
+  }
+
+  const changeSlideLeft = () => {
+    if(index <= 0) return
+    slider.removeEventListener("transitionend", moveAllSliderLeft);
+    slider.removeEventListener("transitionend", moveAllSliderRigth);
+    if (index > 0) {
+      slider.style.transition = "transform .6s ease";
+      index--;
+      slider.style.transform = `translateX(-${index * 20}%)`;
+      slider.addEventListener("transitionend", moveAllSliderLeft);
+    }
+  };
 
   const changeSlideRight = () => {
-    if(indexSLide > slides_number - 2) {
-      
-      for(let i=0;i< slides_number ;i++) {
-        sliderRef.current?.children[i].classList.remove('active');
-      }
-      indexSLide = 0;
-      sliderRef.current?.children[indexSLide].classList.add('active');
-    }else {
-      for(let i=0;i< slides_number ;i++) {
-        sliderRef.current?.children[i].classList.remove('active');
-      }
-      indexSLide++;
-      sliderRef.current?.children[indexSLide].classList.add('active');
+    if(index >= slides_number - 1) return
+    slider.removeEventListener("transitionend", moveAllSliderLeft);
+    slider.removeEventListener("transitionend", moveAllSliderRigth);
+    if (index < slides_number - 1) {
+      slider.style.transition = "transform .6s ease";
+      index++;
+      slider.style.transform = `translateX(-${index * 20}%)`;
+      slider.addEventListener("transitionend", moveAllSliderRigth);
     }
-  }
+  };
 
-  useEffect(()=> {
-    sliderRef.current?.children[indexSLide].classList.add('active');
-    setslides_number(sliderRef.current?.children.length as number);
-  },[])
+  useEffect(() => {
+    if (sliderRef.current) {
+      sliderRef.current.style.transform = `translateX(-${index * 20}%)`;
+    }
+    setSlides_number(sliderRef.current?.children.length as number);
+  }, []);
 
-    return (
-      <div
-        className="slider_c_c"
-        style={{ backgroundImage: `url(${BGImage})` }}
-      >
+  return (
+    <div className="slider_c_c" style={{ backgroundImage: `url(${BGImage})` }}>
+      <div className="container">
         <div className="slides_container" ref={sliderRef}>
+          <div className="slide_last">
+            <div className="img_container">
+              <img src={PizzaImg} alt="" />
+            </div>
+            <h1 className="title_slide">Pizza Americana 3</h1>
+            <span className="price_slide">S/.30.00</span>
+            <p className="description_slide">
+              Curabitur rutrum erat eu tincidunt bibendum. Sed sollicitudin
+              tempus nisl a porta. Sed euismod varius justo, non venenatis
+              lacus. Sed felis elit, elementum vitae vestibulum id, malesuada
+              vel leo.
+            </p>
+          </div>
+
           <div className="slide">
             <div className="img_container">
               <img src={PizzaImg} alt="" />
@@ -96,11 +116,30 @@ export const SliderComponent = () => {
               vel leo.
             </p>
           </div>
-        </div>
-        <div className="controls">
-          <button className="btn_left" onClick={changeSlideLeft}><ArrowSvg/></button>
-          <button className="btn_rigth" onClick={changeSlideRight}><ArrowSvg/></button>
+
+          <div className="slide_first">
+            <div className="img_container">
+              <img src={PizzaImg} alt="" />
+            </div>
+            <h1 className="title_slide">Pizza Americana</h1>
+            <span className="price_slide">S/.30.00</span>
+            <p className="description_slide">
+              Curabitur rutrum erat eu tincidunt bibendum. Sed sollicitudin
+              tempus nisl a porta. Sed euismod varius justo, non venenatis
+              lacus. Sed felis elit, elementum vitae vestibulum id, malesuada
+              vel leo.
+            </p>
+          </div>
         </div>
       </div>
-    );
-}
+      <div className="controls">
+        <button className="btn_left" onClick={changeSlideLeft}>
+          <ArrowSvg />
+        </button>
+        <button className="btn_rigth" onClick={changeSlideRight}>
+          <ArrowSvg />
+        </button>
+      </div>
+    </div>
+  );
+};
